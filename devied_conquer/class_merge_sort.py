bcee18e416e1547a36bb0268d7597242e400fd8a
koo <koo9609@naver.com>
2026-03-11 17:48:03 +0900

    0311. 병합정렬 직관코드

--- devied_conquer/class_merge_sort.py
+++ devied_conquer/class_merge_sort.py
@@ -1,3 +1,22 @@
+
+# 1. 분할하는 과정
+# - depth: 리스트의 길이가 1이 되면 끝
+# -branch: 왼쪽과 오른쪽으로 리스트를 분할(2개)
+def merge_sort(li):
+    if len(li) == 1:
+        return li
+
+    mid = len(li) //2
+    left = li[:mid]     #왼쪽 절반 리스트
+    right = li[mid:]    # 오른쪽 절반 리스트
+#-----------------------------------------
+    left_list = merge_sort(left)
+    right_list= merge_sort(right)
+
+    merge_list = merge(left_list, right_list)
+    return merge_list
+
+
 # 2. 병합하는 과정(이때, 정렬하면서 병합)
 # - 왼쪽, 오른쪽 리스트 중 작은 원소부터 정다 리스트에 추가하자
 def merge(left, right):
@@ -25,25 +44,6 @@ def merge(left, right):
 
 
     return result
-# 1. 분할하는 과정
-# - depth: 리스트의 길이가 1이 되면 끝
-# -branch: 왼쪽과 오른쪽으로 리스트를 분할(2개)
-def merge_sort(li):
-    if len(li) == 1:
-        return li
-
-    mid = len(li) //2
-    left = li[:mid]     #왼쪽 절반 리스트
-    right = li[mid:]    # 오른쪽 절반 리스트
-
-    left_list = merge_sort(left)
-    right_list= merge_sort(right)
-
-    merge_list = merge(left_list, right_list)
-    return merge_list
-
-
-
 arr=[69, 10, 30, 2, 16, 8, 31, 22]
 sorted_arr = merge_sort(arr)
 print(sorted_arr)
